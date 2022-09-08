1259a18f84fe2e3a67d4be157836cfa497c93fa6
ShadowFoxVA <trymstorhaug@gmail.com>
2022-09-08 19:39:00 +0200

    Changed code entirely

--- main.py
+++ main.py
@@ -6,23 +6,24 @@ secret_number = random.randint(0,10)
 # start of program, asking if you want to play a game
 print("Hi, what's your name")
 name = input("Enter name here: ")
-print("Well," {name} "would you like to guess a number")
+print("Well, " + name + " would you like to guess a number between 1 and 10")
 
 # 
-for guess_taken in range(0,6):
+max = 6
+for attempts in range(1, max):
     print("Take a guess")
     guess= int(input())
 
-    if guess < secret_number:
-        print("Your guess is too low, try again")
+    if guess <= 0 or guess > 10:
+        print("That's not a valid number, you have " + str(max - attempts) + " left" )
+    elif guess < secret_number:
+        print("Your guess is too low, you have " + str(max - attempts) + " left")
     elif guess > secret_number:
-        print("Your guess is too high, try again")
-    elif guess <= 0:
-        print("That's not a valid number")
+        print("Your guess is too high, you have " + str(max - attempts) + " left")
     else:
         break
 
 if guess == secret_number:
-    print("You guessed right in " + str(guess_taken) "turns {name})
+    print("You guessed right in " + str(attempts) + " turns " + name)
 else:
-    print("Wrong number, the one I was thinkg of was" + str(secret_number))
+    print("Wrong number, the one I was thinkg of was " + str(secret_number))
